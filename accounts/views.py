9a8f9f8b32bf331ce0d7e1968939e74954c9cb07
KyungMin.Ko <rhrudals312@naver.com>
2024-08-26 13:39:20 +0900

    ✨ 로그인, 로그아웃, 회원가입 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
+
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView, LogoutView
 
 # Create your views here.
+def index(request):
+    return render(request, 'layout/layouts.html')
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'accounts/signup.html'
+    
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+    
+
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    
+
+class CustomLogoutView(LogoutView):
+    template_name = 'accounts/logout.html'
+    
